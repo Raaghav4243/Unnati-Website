dcc64e77defd5f7cfad750e21702f029d68bd4c0
AnshulSatija <55019998+Anshul2707@users.noreply.github.com>
2021-01-09 19:10:17 +0530

    teacher-navbar-sidenav-updated

---
+++ frontend/src/teacher/components/teacher-dashboard-navbar/teacher-dashboard-navbar.component.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import GoToHomePageButton from '../go-to-homepage-button-link/go-to-homepage-button-link.component';
+
+import {
+  NavbarContainer,
+  NavbarLeft,
+  NavbarRight,
+} from './teacher-dashboard-navbar.styles';
+
+const TeacherDashboardNavbar = () => {
+  return (
+    <NavbarContainer>
+      <GoToHomePageButton />
+    </NavbarContainer>
+  );
+};
+
+export default TeacherDashboardNavbar;
