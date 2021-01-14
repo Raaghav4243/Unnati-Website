9b54e7969fbb217be8a4f7b3c0db93b24c88dfb6
raaghav raj maiya <raaghav.4243@gmail.com>
2021-01-15 01:35:38 +0530

    Entreprenuer

---
+++ frontend/src/entreprenuer/redux/users/user.reducer.js
@@ -0,0 +1,78 @@
+import { UserActionTypes } from './user.types';
+
+const INITIAL_STATE = {
+  currentUser: {
+    username: null,
+    email: null,
+    _id: null,
+    cafe: null,
+    firstName: null,
+    lastName: null,
+    phoneNumber: null,
+    role: null,
+  },
+  isFetching: false,
+  errorMessage: undefined,
+  userIsUpdating: false,
+  updateConformation: null,
+};
+
+const userReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case UserActionTypes.SET_CURRENT_USER:
+      return {
+        ...state,
+        currentUser: action.payload,
+      };
+    case UserActionTypes.FETCH_USER_START:
+      return {
+        ...state,
+        isFetching: true,
+      };
+    case UserActionTypes.FETCH_USER_SUCCESS:
+      const {
+        username,
+        email,
+        _id,
+        lastName,
+        firstName,
+        phoneNumber,
+        role,
+        cafe,
+      } = action.payload;
+      return {
+        ...state,
+        isFetching: false,
+        username: username,
+        email: email,
+        _id:_id,
+        lastName: lastName,
+        firstName: firstName,
+        phoneNumber: phoneNumber,
+        role: role,
+        cafe: cafe
+      };
+    case UserActionTypes.FETCH_USER_FAILURE:
+      return {
+        ...state,
+        isFetching: false,
+        errorMessage: action.payload,
+      };
+    case UserActionTypes.UPDATE_USER_START:
+      return {
+        ...state,
+        userIsUpdating: true,
+      };
+    case UserActionTypes.UPDATE_USER_SUCCESS:
+      return {
+        ...state,
+        userIsUpdating: false,
+        updateConformation: action.payload,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default userReducer;
